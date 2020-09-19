0fac52a80e0e6fdf1a1802b410fc925475b24423
99002661 <71481321+99002661@users.noreply.github.com>
2020-09-19 14:29:16 +0530

    Update header.h

--- inc/header.h
+++ inc/header.h
@@ -1,1 +1,14 @@
+#ifndef ___header_H__
+#define ___header_H__
+
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+void calculator_operations();
+
+endif
 
